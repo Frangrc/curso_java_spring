b1a0c7fd9a9b2eddaa95e640d9003b77d6ab73dc
Paco <franciscogrc92@gmail.com>
2019-05-07 14:07:44 +0200

    Archivos java primer dia de clase, tucanes, estructuras...

---
+++ 01_EjemploJava/src/pkg01_ejemplojava/Tucan.java
@@ -0,0 +1,23 @@
+
+package pkg01_ejemplojava;
+
+public class Tucan {
+   String[] colores;
+   float longitud;
+   int edad;
+  
+   public Tucan(){
+   this.edad=0;
+   longitud=5f;
+   colores=new String[]{
+   "blanco", "marron"
+   };
+   }
+   
+   
+   public void piar(){
+
+System.out.println("Pio Pio" + edad + " años");
+}
+
+}
