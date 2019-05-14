942683fbc264f397cf8b11fbe426887549870709
Paco <franciscogrc92@gmail.com>
2019-05-14 18:01:33 +0200

    avtual martes final

--- 03_LecturaClientes/src/com/sinensia/ManipularFichero.java
+++ 03_LecturaClientes/src/com/sinensia/ManipularFichero.java
@@ -9,6 +9,7 @@ import static com.sinensia.pruebas.Polimorfismo.clientes;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 //import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -27,6 +28,8 @@ public class ManipularFichero {
 
         File ejerRutaRelativa = new File("Ejercicio ficheros.txt");
         Scanner escaner = null;
+        HashMap<Integer, Cliente> mapDat=new HashMap<Integer, Cliente>();
+        
         System.out.println("Leyendo ejercicio ficheros");
         try {
             escaner = new Scanner(ejerRutaRelativa);
@@ -36,9 +39,36 @@ public class ManipularFichero {
                 String linea = escaner.nextLine();
 
                 System.out.println(">>>>>>>> \"" + linea + "\"");
-                for(String dato : linea.split(",")){
-                System.out.println("Campo:" + dato.trim());
+                
+                try{
+                
                 }
+                catch(Exception ex){
+                
+                    System.err.println("Error en parseo" + getMessage());
+                }
+                String campos[]=linea.split(",");
+                Cliente cliente;
+                if(campos.length==4 || campos.length==5){
+                
+             
+                long id= Integer.parseInt(campos[0]);
+                String nombre=campos[1];
+                String email=campos[2];
+                boolean activo=campos[3].equals("true") ? true : false; 
+                int duracionMax=0;
+                if(campos.length==5){
+                    duracionMax=Integer.parseInt(campos[4]);
+                    cliente= new ClienteInvitado(id, nombre, email);
+                    ((ClienteInvitado) cliente).setDuracionMax(duracionMax);
+                }
+                else{
+                    cliente= new ClienteInvitado(id, nombre,email);
+                }
+                cliente.setActivo(activo);
+                }
+                
+                mapDat.put(id,cliente);
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ManipularFichero.class.getName()).log(Level.SEVERE, null, ex);
